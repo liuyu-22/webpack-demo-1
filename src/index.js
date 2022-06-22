7049844646bb78d947ceef368680ca7ce9f06bb5
liuyu-22 <729467909@qq.com>
2022-06-22 17:48:56 +0800

    updata

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import x from "./x.js";
 console.log("x");
 console.log("hi");
+
+webpack.config.js
+
+module.exports = {
+  module: {
+    rules: [
+       {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader',]
+       },
+    ],
+  },
+};
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        let fn = module.default
+        fn()
+        // 也可以直接 module.default()
+    }, ()=>{
+        console.log('加载失败')
+    })
+}
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,4 +14,24 @@ module.exports = {
       title: "lynn喜欢Isabella22",
       template: "src/assets/index.html",
     }),],
+
+    module: {
+      rules: [
+          {
+              test: /\.s[ac]ss$/i,
+              use: [
+                // Creates `style` nodes from JS strings
+                  'style-loader',
+                 // Translates CSS into CommonJS
+                  'css-loader',
+                  {	
+                    // Compiles Sass to CSS
+                      loader: 'sass-loader',
+                      options: {
+                          implementation: require('dart-sass'),
+                      }
+                  }
+              ],
+          }
+      ]}
 };
